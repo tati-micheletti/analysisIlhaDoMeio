ab62d51dea8e285c2a7f25cdfebed10ebc3fe461
Tati Micheletti <tati.micheletti@gmail.com>
2023-10-24 14:35:24 +0200

    Added test files and pdf

---
+++ test_ZI.R
@@ -0,0 +1,25 @@
+# TEST FOR ZERO INFLATION
+
+test_ZI <- function(dts){
+  pois_data <- dts
+  lambda_est <- mean(pois_data)
+  p0_tilde <- exp(-lambda_est)
+  p0_tilde
+  n0 <- sum(1*(!(pois_data >0)))
+  n <- length(pois_data)
+  # number of observtions 'expected' to be zero
+  expectedZeros <- n*p0_tilde
+  realZeros <- table(pois_data)["0"]
+  message(paste0("Real zeros over expected zeros by a factor of ", 
+                 realZeros/expectedZeros)) # More realZeros than expected zeros, likely ZI))
+  #now lets perform the JVDB score test 
+  numerator <- (n0 -n*p0_tilde)^2
+  denominator <- n*p0_tilde*(1-p0_tilde) - n*lambda_est*(p0_tilde^2)
+  test_stat <- numerator/denominator
+  pvalue <- pchisq(test_stat,df=1, ncp=0, lower.tail=FALSE)
+  if (pvalue < 0.05){
+    "Data likely zero inflated"
+  } else {
+    "Data likely not zero inflated"
+  }
+}
